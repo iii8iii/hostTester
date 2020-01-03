ce2150f18aea551d9672409739c131bda3c29c1b
ocy <zichengou@gmail.com>
2020-01-03 09:55:05 +0800

    init

---
+++ index.js
@@ -0,0 +1,56 @@
+const Hapi = require('@hapi/hapi');
+const ytdl = require('ytdl-core');
+const Ammo = require('@hapi/ammo');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+})
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+        return `<video src="/v" controls="controls">\
+        您的浏览器不支持 video 标签。\
+        </video>`
+    }
+})
+
+
+server.route({
+    method: 'GET',
+    path: '/v',
+    handler: (request, h) => {
+
+        return ytdl('3PWMgQFVVO8');
+        // let requestRange = request.headers.range;
+        // let range = Ammo.header(requestRange, streamSize)[0]
+        // console.log('range:', range)
+        // const start = range.from;
+        // const end = range.to;
+        // const stream = new Ammo.Clip(range)
+        // video.pipe(stream)
+        // return h
+        //     .response(stream)
+        //     .code(206)
+        //     .header('Content-Range', 'bytes ' + start + '-' + end + '/' + streamSize)
+        //     .header('Content-Length', start == end ? 0 : (end - start + 1))
+        //     .header('Content-Type', 'video/mp4')
+        //     .header('Accept-Ranges', 'bytes')
+        //     .header('Cache-Control', 'no-cache')
+    }
+})
+
+
+const init = async () => {
+    await server.start()
+    console.log(`server running at:${server.info.uri}`)
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err)
+    process.exit(1)
+})
+
+init()
